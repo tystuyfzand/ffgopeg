76ff3e5d28812461d88e4bbaa27105617b808497
paulhobbel <itsdizzy40@gmail.com>
2020-05-19 00:33:03 +0200

    Reverted some C.longlong -> C.long

--- avformat/context_struct.go
+++ avformat/context_struct.go
@@ -265,7 +265,7 @@ func (ctxt *FormatContext) SkipInitialBytes() int64 {
 //
 // C-Variable: AVFormatContext::start_time
 func (ctxt *FormatContext) SetStartTime(val int64) {
-	ctxt.start_time = C.longlong(val)
+	ctxt.start_time = C.long(val)
 }
 
 //
--- avutil/mathematics.go
+++ avutil/mathematics.go
@@ -15,11 +15,11 @@ func MakeQ(num, den int) Rational {
 //
 // C-Function: av_rescale
 func Rescale(a, b, c int64) int64 {
-	return int64(C.av_rescale(C.longlong(a), C.longlong(b), C.longlong(c)))
+	return int64(C.av_rescale(C.long(a), C.long(b), C.long(c)))
 }
 
 //
 // C-Function: av_rescale_q
 func RescaleQ(a int64, bq Rational, cq Rational) int64 {
-	return int64(C.av_rescale_q(C.longlong(a), (C.struct_AVRational)(bq), (C.struct_AVRational)(cq)))
+	return int64(C.av_rescale_q(C.long(a), (C.struct_AVRational)(bq), (C.struct_AVRational)(cq)))
 }
